f8b2370a87b4cfd0749eba9af70b772b39fc8a31
Kimlee-cmd <85122673+Kimlee-cmd@users.noreply.github.com>
2022-08-18 02:46:48 +0800

    Add files via upload

--- app/src/main/java/com/github/catvod/spider/Yisou.java
+++ app/src/main/java/com/github/catvod/spider/Yisou.java
@@ -4,86 +4,73 @@ import android.content.Context;
 import com.github.catvod.crawler.Spider;
 import com.github.catvod.crawler.SpiderDebug;
 import com.github.catvod.utils.okhttp.OkHttpUtil;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
 import java.net.URLEncoder;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class Yisou extends Spider {
-    private static final Pattern aliyun = Pattern.compile("(https://www.aliyundrive.com/s/[^\"]+)");
-    private PushAgent pushAgent;
+    private PushAgent z;
 
-
-    public String detailContent(List<String> list) {
+    public String searchContent(String str, boolean z) {
+        String str2 = "list";
         try {
-            return pushAgent.detailContent(list);
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("https://yiso.fun/api/search?name=");
+            stringBuilder.append(URLEncoder.encode(str));
+            stringBuilder.append("&from=ali");
+            JSONArray jSONArray = new JSONObject(z(o.E(stringBuilder.toString(), mF()))).getJSONObject("data").getJSONArray(str2);
+            JSONArray jSONArray2 = new JSONArray();
+            for (int i = 0; i < jSONArray.length(); i++) {
+                JSONObject jSONObject = jSONArray.getJSONObject(i);
+                String string = jSONObject.getJSONArray("fileInfos").getJSONObject(0).getString("fileName");
+                String string2 = jSONObject.getString("gmtCreate");
+                String string3 = jSONObject.getString("url");
+                JSONObject jSONObject2 = new JSONObject();
+                jSONObject2.put("vod_id", string3);
+                jSONObject2.put("vod_name", string);
+                jSONObject2.put("vod_pic", "https://inews.gtimg.com/newsapp_bt/0/13263837859/1000");
+                jSONObject2.put("vod_remarks", string2);
+                jSONArray2.put(jSONObject2);
+            }
+            JSONObject jSONObject3 = new JSONObject();
+            jSONObject3.put(str2, jSONArray2);
+            return jSONObject3.toString();
         } catch (Exception e) {
             SpiderDebug.log(e);
+            return "";
         }
-        return "";
     }
 
+    protected static HashMap<String, String> mF() {
+        HashMap hashMap = new HashMap();
+        hashMap.put("User-Agent", "Mozilla/5.0 (Linux; Android 12; V2049A Build/SP1A.210812.003; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/103.0.5060.129 Mobile Safari/537.36");
+        hashMap.put("Referer", "https://yiso.fun/");
+        return hashMap;
+    }
 
     public void init(Context context, String str) {
         super.init(context, str);
-        pushAgent = new PushAgent();
-        pushAgent.init(context,str);
+        PushAgent pushAgent = new PushAgent();
+        this.z = pushAgent;
+        pushAgent.init(context, str);
     }
 
-
-    public String playerContent(String str, String str2, List<String> list) {
-        return pushAgent.playerContent(str, str2, list);
-    }
-
-    private Pattern regexVid = Pattern.compile("(\\S+)");
-    public String searchContent(String key, boolean quick) {
+    public String detailContent(List<String> list) {
         try {
-            if (quick)
-                return "";
-            String url = "https://yiso.fun/info?searchKey=" + URLEncoder.encode(key)+"&from=ali";
-            Document doc = Jsoup.parse(OkHttpUtil.string(url, null));
-            JSONObject result = new JSONObject();
-
-            JSONArray videos = new JSONArray();
-            // Elements list = doc.select("ul.newsList>li>div.li_con");
-            Elements list = doc.select("div.col-md-8>div.row-cards");
-            for (int i = 0; i < list.size(); i++) {
-                Element vod = list.get(i);
-                String title = "";
-                Elements titlelist = vod.select("h3.card-title>a>span");
-                for(int k = 0; k < titlelist.size(); k++){
-                    title+=titlelist.get(k).text()
-                }
-                String cover = "";
-                String remark = vod.selectFirst("div.align-items-center>p.text-muted").text();
-                Matcher matcher = regexVid.matcher(vod.select("h3.card-title>a").attr("href"));
-                if (!matcher.find())
-                    continue;
-                String id = matcher.group(1);
-                JSONObject v = new JSONObject();
-                //v.put("vod_id", vod.select("h3.card-title>a").attr("href"));
-                v.put("vod_id", id);
-                v.put("vod_name", title);
-                v.put("vod_pic", "https://inews.gtimg.com/newsapp_bt/0/13263837859/1000");
-                v.put("vod_remarks", remark);
-                videos.put(v);
-            }
-
-            result.put("list", videos);
-            return result.toString();
+            return this.z.detailContent(list);
         } catch (Exception e) {
             SpiderDebug.log(e);
+            return "";
         }
-        return "";
+    }
+
+    public String playerContent(String str, String str2, List<String> list) {
+        return this.z.playerContent(str, str2, list);
+    }
+
+    protected String z(String str) {
+        return str;
     }
 }
